e5730a14c01f283dfaa52625ff62b5a30c7a57b8
xBr <84455199+xB-r@users.noreply.github.com>
2024-08-12 11:05:58 +0400

    Main v1.0 release
    API has been tested and completed.

---
+++ tesseract_api.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from PIL import Image
+import pytesseract
+import cv2
+import base64
+import io
+import numpy as np
+
+app = FastAPI()
+
+
+class ImageData(BaseModel):
+    image: str
+
+
+def base64_2_image(base64_image_data: str) -> Image.Image:
+    try:
+        image_data = base64.b64decode(base64_image_data)
+        image_file = Image.open(io.BytesIO(image_data))
+        return image_file
+    except Exception as e:
+        raise ValueError("Invalid base64 image data") from e
+
+
+@app.post("/ocr")
+async def perform_ocr(data: ImageData):
+    try:
+        image = base64_2_image(data.image)
+        image_np = np.array(image)
+        gray_image = cv2.cvtColor(image_np, cv2.COLOR_RGB2GRAY)
+        image_pillow = Image.fromarray(gray_image)
+        text = pytesseract.image_to_string(image_pillow, lang='eng')
+        text_lines = text.splitlines()
+
+        return {"recognized_texts": text_lines}
+
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
+
+    except Exception:
+        raise HTTPException(status_code=500, detail="An error occurred during OCR processing.")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ tesseract_api_interface.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import base64
+import os
+
+
+def encode_image_to_base64(image_path: str) -> str:
+    with open(image_path, 'rb') as image_file:
+        base64_image = base64.b64encode(image_file.read()).decode('utf-8')
+    return base64_image
+
+
+def save_json_to_file(data: dict, output_file: str):
+    with open(output_file, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+        print(f"Results saved to {output_file}")
+
+
+def main():
+    image_path = input("Enter image directory: ")
+
+    if not os.path.exists(image_path):
+        print(f"Error: File '{image_path}' does not exist.")
+        return
+
+    base64_image = encode_image_to_base64(image_path)
+    payload = {'image': base64_image}
+
+    try:
+        response = requests.post('http://localhost:8000/ocr', json=payload)
+        response.raise_for_status()
+        result = response.json()
+        save_json_to_file(result, 'ocr_results.json')
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == "__main__":
+    main()
